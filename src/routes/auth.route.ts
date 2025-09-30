f51b6cf3c80403dc13a189f8030e101f0d597d6b
raffinauvall <raffinauvaltaqy@gmail.com>
2025-10-01 02:13:06 +0700

    feat: add authentication route

---
+++ src/routes/auth.route.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { signUp, signIn } from "../controllers/auth.controller.js";
+
+const router = Router();
+
+router.post("/sign-up", signUp);
+router.post("/sign-in", signIn);
+
+export default router;
