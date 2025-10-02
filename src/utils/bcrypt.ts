7ec5a7a6ea50399a4b6b619f72089f4c911175bc
raffinauvall <raffinauvaltaqy@gmail.com>
2025-10-03 03:43:37 +0700

    refactor: change func

--- src/utils/bcrypt.ts
+++ src/utils/bcrypt.ts
@@ -1,4 +1,14 @@
 import bcrypt from "bcrypt";
 
-export const hashPassword = (password: string) => bcrypt.hashSync(password, 10);
-export const comparePassword = (password: string, hash: string) => bcrypt.compareSync(password, hash);
+const SALT_ROUNDS = 10
+
+export const hashPassword = async (password: string): Promise<string> => {
+    return await bcrypt.hash(password, SALT_ROUNDS)
+}
+
+export const comparePassword = async (
+    password: string,
+    hashedPassword: string
+): Promise<boolean> => {
+    return await bcrypt.compare(password, hashedPassword)
+}
--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -2,6 +2,14 @@ import jwt from "jsonwebtoken"
 
 const JWT_SECRET = process.env.JWT_SECRET || "secretkey";
 
-export const generateToken = (userId: number) => {
+export const generateToken = (userId: number): string => {
     return jwt.sign({ userId }, JWT_SECRET, { expiresIn: "1d" })
 };
+
+export const verifyToken = (token: string): any => {
+    try {
+        return jwt.verify(token, JWT_SECRET) as {userId: number};
+    } catch (err) {
+        return null;
+    }
+}
