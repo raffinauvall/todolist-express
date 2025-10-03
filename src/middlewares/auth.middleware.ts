12a9e6723bd1c1a3655c36b25ee381fe9c41001f
raffinauvall <raffinauvaltaqy@gmail.com>
2025-10-04 00:24:06 +0700

    refactor: fixing middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,30 +1,24 @@
-import type { Request, Response } from "express";
-import type { NextFunction } from "express-serve-static-core";
-import jwt from "jsonwebtoken";
-
-
-const JWT_SECRET = process.env.JWT_SECRET || "secretkey";
+import { Request, Response, NextFunction } from "express";
+import { verifyToken } from "../utils/jwt";
 
 export interface AuthRequest extends Request {
   userId?: number;
 }
 
-export const authMiddleware = (
-  req: AuthRequest,
-  res: Response,
-  next: NextFunction
-) => {
-  const authHeader = req.headers?.authorization;
-  if (!authHeader) {
-    return res.status(401).json({ message: "Unauthorized" });
+export const authMiddleware = (req: AuthRequest, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No token provided."});
   }
 
   const token = authHeader.split(" ")[1];
-  try {
-    const decoded = jwt.verify(token, JWT_SECRET) as { userId: number };
-    req.userId = decoded.userId;
-    next();
-  } catch (err) {
-    return res.status(401).json({ message: "Invalid token" });
+  const decoded = verifyToken(token);
+
+  if (!decoded) {
+    return res.status(401).json({ message: "Invalid or expired token."});
   }
-};
+
+  req.userId = decoded.userId;
+  next();
+}
