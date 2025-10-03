6de066ad689f191fb151463b0067709f3bc3c225
raffinauvall <raffinauvaltaqy@gmail.com>
2025-10-04 00:23:18 +0700

    feat: add route for auth

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,14 @@
-import express, { Application } from "express";
+import express from "express";
 import cors from "cors";
-import morgan from "morgan";
-import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route";
-import taskRoutes from "./routes/task.route";
 
-dotenv.config();
+const app = express();
 
-const app: Application = express();
-const PORT = process.env.PORT || 5000;
-
+// Middleware
 app.use(cors());
-app.use(morgan("dev"));
 app.use(express.json());
 
-app.use("/auth", authRoutes);
-app.use("/tasks", taskRoutes);
+// Routes
+app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+export default app;
