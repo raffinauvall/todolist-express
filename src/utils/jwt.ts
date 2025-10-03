24f1d67b4929b104b2072d980d5f0293b8f494f4
raffinauvall <raffinauvaltaqy@gmail.com>
2025-10-04 00:25:57 +0700

    feat: add payload for clean

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,15 +1,15 @@
-import jwt from "jsonwebtoken"
+import jwt, { JwtPayload } from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET || "secretkey";
+const SECRET = process.env.JWT_SECRET || "secret123"; 
 
-export const generateToken = (userId: number): string => {
-    return jwt.sign({ userId }, JWT_SECRET, { expiresIn: "1d" })
+export const generateToken = (payload: object): string => {
+  return jwt.sign(payload, SECRET, { expiresIn: "1d" });
 };
 
-export const verifyToken = (token: string): any => {
-    try {
-        return jwt.verify(token, JWT_SECRET) as {userId: number};
-    } catch (err) {
-        return null;
-    }
-}
+export const verifyToken = (token: string): JwtPayload | null => {
+  try {
+    return jwt.verify(token, SECRET) as JwtPayload;
+  } catch {
+    return null;
+  }
+};
