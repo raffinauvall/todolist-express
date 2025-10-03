ef8d150b4349cf58dda8ec425dcad87f27f4bdbc
raffinauvall <raffinauvaltaqy@gmail.com>
2025-10-04 00:23:52 +0700

    refactor: fixing controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,33 +1,22 @@
 import { Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
-import { hashPassword, comparePassword } from "../utils/bcrypt.js"
-import { generateToken } from "../utils/jwt.js";
+import { registerService, loginService } from "../services/auth.service";
 
-const prisma = new PrismaClient();
-
-export const signUp = async (req: Request, res: Response) => {
+export const register = async (req: Request, res: Response) => {
+  try {
     const { email, password } = req.body;
-    const hashed = hashPassword(password);
-
-    try {
-        const user = await prisma.user.create({
-            data: {email, password: hashed}
-        });
-        res.json({message: "User Created", userId: user.id})
-    } catch (err) {
-        res.status(400).json({ message: "User already exists" });
-    }
+    const user = await registerService(email, password);
+    res.status(200).json({ message: "User registered successfully", user });
+  } catch (err: any) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
-export const signIn = async (req: Request, res: Response) => {
+export const login = async (req: Request, res: Response) => {
+  try {
     const { email, password } = req.body;
-
-    const user = await prisma.user.findUnique({ where: {email} });
-    if (!user) return res.status(400).json({ message: "User not found" });
-
-    const isValid = comparePassword(password, user.password);
-    if (!isValid) return res.status(401).json({ message: "Invalid Passowrd" });
-
-    const token = generateToken(user.id);
-    res.json(token);
-}
+    const { user, token } = await loginService(email, password);
+    res.status(200).json({ message: "Login successful", token, user });
+  } catch (err: any) {
+    res.status(400).json({ error: err.message });
+  }
+};
