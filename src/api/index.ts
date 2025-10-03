5560621839afd3e10ac6aca9130eb5664b180139
raffinauvall <raffinauvaltaqy@gmail.com>
2025-10-04 00:23:37 +0700

    feat: add server application

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,15 +1,17 @@
 import express from "express";
 import cors from "cors";
-import taskRoutes from "../routes/task.route"
-import authRoutes from "../routes/auth.route"
+
+import authRoutes from "../routes/auth.route";   // <── ini
+import taskRoutes from "../routes/task.route";   // <── ini
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// prefix route
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
 app.listen(3000, () => {
-    console.log("Server running at http://localhost:3000")
+  console.log("Server running at http://localhost:3000");
 });
